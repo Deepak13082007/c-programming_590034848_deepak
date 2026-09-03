b351c9101c451ccf5925aa0d4d55688ab82ec372
Deepak13082007 <DEEPAK.34848@stu.upes.ac.in>
2026-09-03 17:35:33 +0530

    Add files via upload

---
+++ day25_q2.c
@@ -0,0 +1,43 @@
+/* Write a program to print the following pattern:
+*****
+ ****
+  ***
+   **
+    *
+
+
+Sample Test Cases:
+Input 1:
+
+Output 1:
+*****
+ ****
+  ***
+   **
+    *
+
+Input 2:
+
+Output 2:
+Note: Spaces indicate indentation.
+
+*/
+#include <stdio.h>
+
+int main() {
+    for (int i = 0; i < 5; i++) {
+        // Print spaces
+        for (int j = 0; j < i; j++) {
+            printf(" ");
+        }
+
+        // Print stars
+        for (int j = i; j < 5; j++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
