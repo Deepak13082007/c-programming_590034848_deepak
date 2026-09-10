81c12140206b975e8742a0b44f040ee28ee45b9c
Deepak13082007 <DEEPAK.34848@stu.upes.ac.in>
2026-09-10 19:58:58 +0530

    Add files via upload

---
+++ day32_q2.c
@@ -0,0 +1,45 @@
+// Find the digit that occurs the most times in an integer number.
+
+/*
+Sample Test Cases:
+Input 1:
+112233
+Output 1:
+1
+
+Input 2:
+887799
+Output 2:
+7
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    long long num;
+    int freq[10] = {0};
+    int digit, i, max = 0, result = 0;
+
+    printf("Enter an integer number: ");
+    scanf("%lld", &num);
+
+    // Count frequency of each digit
+    while (num > 0) {
+        digit = num % 10;
+        freq[digit]++;
+        num = num / 10;
+    }
+
+    // Find the digit with maximum frequency
+    for (i = 0; i < 10; i++) {
+        if (freq[i] > max) {
+            max = freq[i];
+            result = i;
+        }
+    }
+
+    printf("Digit occurring most times: %d\n", result);
+
+    return 0;
+}
